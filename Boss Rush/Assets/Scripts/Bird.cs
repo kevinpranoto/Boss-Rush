df623834540d79242b49fec92dc9e4a650c96a47
wiwinewijaya <wiwinwijaya97@yahoo.com>
2017-11-02 12:00:51 -0700

    Unity Project
    Created the unity project and copied over scripts

---
+++ Boss Rush/Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : Enemy {
+    public float rotationSpeed;
+
+    private Vector3 pivot;
+    private bool tracked;
+    private float radius = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        baseStart();
+        //pivot = player.transform.position - transform.position;
+        tracked = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        baseUpdate();
+
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, player.transform.position) < 5.0f)
+        {
+            if (!tracked)
+            {
+                pivot = transform.position - player.transform.position;
+                tracked = true;
+            }
+            pivot = (Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.forward) * pivot);
+
+            transform.position = player.transform.position + pivot;
+        }   
+    }
+
+}
---
+++ Boss Rush/Assets/Scripts/BossOneController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossOneController : MonoBehaviour {
+    public float health;
+    public List<GameObject> attacks;
+
+    public float phaseOneTime;
+    public Transform lanceSpawn;
+
+    public float phaseTwoTime;
+    public Transform player;
+    public float arrowOffset;
+
+    public float phaseThreeTime;
+    public Transform birdSpawn;
+
+    private Vector2 max;
+
+	// Use this for initialization
+	void Start () {
+        max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        StartCoroutine(BossRoutine());
+	}
+
+    IEnumerator BossRoutine()
+    {
+        while (health > 70.0f)
+        {
+            phaseOne();
+            yield return new WaitForSeconds(phaseOneTime);
+        }
+
+        while (health > 30.0f)
+        {
+            phaseTwo();
+            yield return new WaitForSeconds(phaseTwoTime);
+        }
+
+        while (health > 0.0f)
+        {
+            phaseThree();
+            yield return new WaitForSeconds(phaseThreeTime);
+        }
+    }
+
+    void phaseOne()
+    {
+        Instantiate(attacks[0], lanceSpawn.position, lanceSpawn.rotation);
+    }
+
+    void phaseTwo()
+    {
+        Instantiate(attacks[1], new Vector3(player.position.x, max.y + arrowOffset, 0), new Quaternion(0, 0, 0, 0));
+    }
+
+    void phaseThree()
+    {
+        Instantiate(attacks[2], birdSpawn.position, birdSpawn.rotation);
+    }
+}
