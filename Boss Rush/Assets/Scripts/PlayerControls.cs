ad314687bd95f313fa73a8f24042e6a693ee0f6c
Kevin Pranoto <kevinpranoto97@gmail.com>
2017-11-07 23:22:56 -0800

    Added bullet prefabs and added firing in PlayerControls script

--- Boss Rush/Assets/Scripts/PlayerControls.cs
+++ Boss Rush/Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,9 @@ public class PlayerControls : MonoBehaviour {
 	private Rigidbody2D rb2d;
 	public float moveSpeed = 7;
 	public float jumpStrength = 7;
+	public GameObject leftBullet, rightBullet;
+	public Transform firePos;
+	private bool facingRight = true;
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<Transform> ();
@@ -29,6 +32,20 @@ public class PlayerControls : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space))
 		{
 			rb2d.AddForce (new Vector3 (0, 1, 0) * jumpStrength);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Z)) 
+		{
+			Fire ();
+		}
+	}
+
+	void Fire()
+	{
+		if (facingRight) {
+			Instantiate (rightBullet, firePos.position, firePos.rotation);
+		} else {
+			Instantiate (leftBullet, firePos.position, firePos.rotation);
 		}
 	}
 }
