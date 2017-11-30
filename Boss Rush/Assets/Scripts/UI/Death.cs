500b19c0b05b8ff16e30e72e3a055903dd751d8b
wiwinewijaya <wiwinwijaya97@yahoo.com>
2017-11-30 13:14:02 -0800

    Added death screen
    changed title screen so it goes to balancing scene

---
+++ Boss Rush/Assets/Scripts/UI/Death.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	private GameObject menu;
+	private PlayerControls player;
+
+	void Awake()
+	{
+		menu = GameObject.Find("Death Menu");
+		menu.SetActive(false);
+		player = GameObject.Find("Player").GetComponent<PlayerControls>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (player.isPlayerDead() && menu.activeInHierarchy == false)
+		{
+			Pause();
+		}
+	}
+
+	public void Pause()
+	{
+		if (menu.activeInHierarchy == false)
+		{
+			menu.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else
+		{
+			menu.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+}
